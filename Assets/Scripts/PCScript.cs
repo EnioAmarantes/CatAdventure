c826eaff4949302847a74d9d2380a7c0fb71120f
unknown <enio.amarantes@gmail.com>
2023-04-05 20:49:35 -0300

    Start Animations

--- Assets/Scripts/PCScript.cs
+++ Assets/Scripts/PCScript.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PCScript : MonoBehaviour
 {
     private Rigidbody2D rbd;
+    private Animator anim;
     public float vel;
+    private bool direita = true;
     // Start is called before the first frame update
     void Start()
     {
         vel = 10;
         rbd = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,5 +21,14 @@ public class PCScript : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         rbd.velocity = new Vector2(x * vel, rbd.velocity.y);
+
+        anim.SetBool("movimento", !(x == 0));
+
+        if (direita && x < 0 || !direita && x > 0)
+        {
+            transform.Rotate(new Vector2(0, 180));
+            direita = !direita;
+        }
+
     }
 }
